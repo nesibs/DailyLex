39a8ef6d415cec5fef0400e3da0c39ac90837e70
Nesib Shirinov <sirinovnasib@gmail.com>
2026-05-08 23:06:11 +0400

    DailyLex ready for deploy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,20 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {
-  getFirestore,
-  enableIndexedDbPersistence,
-} from "firebase/firestore";
 
 import {
   getFirestore,
   enableIndexedDbPersistence,
 } from "firebase/firestore";
-
-export const db = getFirestore(app);
-
-enableIndexedDbPersistence(db).catch((error) => {
-  console.log("Offline persistence error:", error.code);
-});
 
 const firebaseConfig = {
   apiKey: "AIzaSyAuS6FQ6ZszevDaVfDCTN86q1rDSFSba9c",
@@ -23,12 +13,13 @@ const firebaseConfig = {
   storageBucket: "dailylex-db7c3.firebasestorage.app",
   messagingSenderId: "517012006678",
   appId: "1:517012006678:web:19e97f41a4fb4bdc99d237",
-  measurementId: "G-FBVRFV4GBJ"
+  measurementId: "G-FBVRFV4GBJ",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
 
 enableIndexedDbPersistence(db).catch((error) => {
